a4164a9a503b58b130ea032c6a4585626eeb5d8f
sanjulablockchain <sanjulablockchain@gmail.com>
2026-08-03 22:40:55 +0530

    Add welcome/about section

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "./components/Hero";
 import { StatsBar } from "./components/StatsBar";
+import { Welcome } from "./components/Welcome";
 
 export function HomePage() {
   return (
     <>
       <Hero />
       <StatsBar />
+      <Welcome />
     </>
   );
 }
